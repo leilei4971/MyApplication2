c5ddee2038b049c757addb6463680b073075e4df
leilei4971 <2560237582@qq.com>
2017-10-19 11:49:37 +0800

    20171019

---
+++ app/src/main/java/com/example/administrator/myapplication/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.administrator.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/10/19.
+ */
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
