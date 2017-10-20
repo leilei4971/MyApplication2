0ace18d3c008444b31413e77973183e19efe29fb
login <2560237582@qq.com>
2017-10-20 09:47:51 +0800

    login

--- app/src/main/java/com/example/administrator/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/LoginActivity.java
@@ -1,9 +1,11 @@
 package com.example.administrator.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by Administrator on 2017/10/19.
@@ -15,5 +17,10 @@ public class LoginActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+    }
+    public void onClick(View v){
+        Intent intent=new Intent( LoginActivity.this,MainActivity.class);
+        startActivity(intent);
     }
 }
